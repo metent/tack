487412a6d3610e42a1a6eee2f678e0438bc6dccc
Rishabh Das <rdas@tutanota.com>
2021-10-30 16:25:53 +0530

    delete legacy code

--- lib/main.dart
+++ lib/main.dart
@@ -16,90 +16,21 @@
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import 'package:flutter/material.dart';
-import 'package:tack/multitree.dart' as multitree;
-import 'package:tack/widgets/textfield.dart';
-import 'package:tack/widgets/tasktree.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-const appBarTitle = "Tack";
+import 'package:tack/widgets/multitree.dart';
+import 'package:tack/widgets/flattree.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(Application());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class Application extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-            textTheme: GoogleFonts.nunitoSansTextTheme(
-          Theme.of(context).textTheme,
-        )),
-        home: Home(),
-        debugShowCheckedModeBanner: false);
-  }
-}
-
-class Home extends StatefulWidget {
-  @override
-  State<Home> createState() => HomeState();
-}
-
-class HomeState extends State<Home> {
-  static TextEditingController textController = TextEditingController();
-  static final taskTree = GlobalKey<TreeNodeWidgetState>();
-
-  bool isVisible = false;
-  String newNodeTitle = "";
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Padding(
-          padding: EdgeInsets.only(left: 10),
-          child: Text(
-            appBarTitle,
-            style: TextStyle(
-              fontSize: 22.0,
-              color: Color(0XFF211551),
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.add),
-            onPressed: () {
-              taskTree.currentState!.parentId = 0;
-              toggleVisibility();
-            },
-          )
-        ],
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          SingleChildScrollView(
-              child: TreeNodeWidget(0, multitree.build(), toggleVisibility,
-                  setVisiblityFalse, taskTree)),
-          CustomTextField(isVisible, setVisiblityFalse)
-        ],
+      title: 'Tack',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Task Tree')),
+        body: MultitreeWidget(child: Flattree()),
       ),
     );
-  }
-
-  void toggleVisibility() {
-    isVisible = !isVisible;
-    textController.text = "";
-    setState(() {});
-  }
-
-  void setVisiblityFalse() {
-    isVisible = false;
-    textController.text = "";
-    setState(() {});
   }
 }
